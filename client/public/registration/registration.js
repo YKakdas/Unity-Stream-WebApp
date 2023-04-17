ed1a628798abb59ba4342b0bc277fead95000f4f
Yasar Kakdas <ykakdas@gmail.com>
2023-04-16 22:42:04 -0400

    Input handling for registration page.

---
+++ client/public/registration/registration.js
@@ -0,0 +1,100 @@
+const form = document.querySelector("form");
+nameField = form.querySelector(".name");
+nameInput = nameField.querySelector("input");
+
+emailField = form.querySelector(".email");
+emailInput = emailField.querySelector("input");
+
+createPasswordField = form.querySelector(".create_password");
+createPasswordInput = createPasswordField.querySelector("input");
+
+confirmPasswordField = form.querySelector(".confirm_password");
+confirmPasswordInput = confirmPasswordField.querySelector("input");
+
+form.onsubmit = (e) => {
+    e.preventDefault(); //Prevent the form from submitting
+
+    //If inputs are empty, then shake them
+    if (nameInput.value == "") {
+        nameField.classList.add("shake", "error");
+    } else {
+        nameField.classList.remove("shake", "error");
+    }
+
+    if (emailInput.value == "") {
+        emailField.classList.add("shake", "error");
+    } else {
+        emailField.classList.remove("shake", "error");
+    }
+
+    if (createPasswordInput.value == "") {
+        createPasswordField.classList.add("shake", "error");
+    } else {
+        createPasswordField.classList.remove("shake", "error");
+    }
+
+    if (confirmPasswordInput.value == "") {
+        confirmPasswordField.classList.add("shake", "error");
+    } else {
+        confirmPasswordField.classList.remove("shake", "error");
+    }
+
+    if (createPasswordInput.value != "" && confirmPasswordInput.value != "") {
+        if (createPasswordInput.value != confirmPasswordInput.value) {
+            confirmPasswordField.classList.add("shake", "error");
+        }
+    }
+
+
+    if (emailInput.value != "") {
+        checkEmail();
+    }
+
+
+    setTimeout(() => { //remove shake class after 500ms
+        emailField.classList.remove("shake")
+        nameField.classList.remove("shake")
+        createPasswordField.classList.remove("shake");
+        confirmPasswordField.classList.remove("shake");
+    }, 500)
+
+    emailInput.onkeyup = () => {
+        checkEmail();
+    }
+
+    nameInput.onkeyup = () => {
+        if (nameInput.value != "") {
+            nameField.classList.remove("error");
+        }
+    }
+
+    createPasswordInput.onkeyup = () => {
+        if (createPasswordInput.value != "") {
+            createPasswordField.classList.remove("error");
+        }
+    }
+
+    confirmPasswordInput.onkeyup = () => {
+        if (createPasswordInput.value == confirmPasswordInput.value) {
+            confirmPasswordField.classList.remove("error");
+        }
+    }
+
+    function checkEmail() {
+        let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; //pattern to validate email
+        if (!emailInput.value.match(pattern)) { //if the pattern doesn't match
+            emailField.classList.add("error");
+            let errorTxt = emailField.querySelector(".error-text");
+            (emailInput.value != "") ? errorTxt.innerText = "Enter a valid email address" : errorTxt.innerText = "Email can't be blank";
+        } else {
+            emailField.classList.remove("error");
+        }
+    }
+
+    if (!emailField.classList.contains("error") && !nameField.classList.contains("error") &&
+        !createPasswordField.classList.contains("error") && !confirmPasswordField.classList.contains("error")
+    ) {
+        window.location.href = "#"; //# is where you want to submit the form data
+        console.log("Form submitted");
+    }
+}
